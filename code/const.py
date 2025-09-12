9c5d3d401a16dfe038ae338b260fc872c1579f1e
CLIENTE <petersontenorio10@gmail.com>
2025-09-12 14:37:07 -0300

    Changing feature

---
+++ code/const.py
@@ -0,0 +1,18 @@
+from pygame.examples.grid import WINDOW_WIDTH
+
+# W
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
+
+# C
+C_ORANGE = (255, 128, 0)
+C_WHITE = (255, 255, 255)
+C_YELLOW = (255, 255, 0)
+
+# M
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW FAME 2P - COMPETITIVE',
+               'SCORE',
+               'EXIT'
+               )
--- code/game.py
+++ code/game.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import pygame as pg
+import pygame
 
+from code.const import WIN_WIDTH, WIN_HEIGHT
 from code.menu import Menu
 
 
 class Game:
     def __init__(self):
-        pg.init()
-        self.window = pg.display.set_mode(size=(600, 480))
+        pygame.init()
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
         while True:
             menu = Menu(self.window)
             menu.run()
             pass
-
-            # Check for all events
-            # for event in pg.event.get():
-            #    if event.type == pg.QUIT:
-             #    pg.quit()  # Close window
-             #    quit()  # End pygame
